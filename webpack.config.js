b81c23f0b59568e44b073c3591912a08f07418fe
sdcmc <sudachimochi@gmail.com>
2019-06-21 00:07:28 +0900

    ReactとWebpackのサンプル追加、プリセット参照しないよう.babelrcを一旦削除

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var debug   = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path    = require('path');
+
+module.exports = {
+  context: path.join(__dirname, ""),
+  entry: "./js/index.js",
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }]
+      }]
+    },
+    output: {
+      path: __dirname + "/js/",
+      filename: "index.min.js"
+    },
+    plugins: debug ? [] : [
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ]
+};
